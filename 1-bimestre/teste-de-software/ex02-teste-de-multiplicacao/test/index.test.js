e712e3b0ffa9727807315032998563125a98f66d
Rosiele Sousa <rosielesousa49@gmail.com>
2026-02-09 11:48:39 +0000

    Finalização da atividade

--- 1-bimestre/teste-de-software/ex02-teste-de-multiplicacao/test/index.test.js
+++ 1-bimestre/teste-de-software/ex02-teste-de-multiplicacao/test/index.test.js
@@ -1,5 +1,5 @@
 const multiplicar = require("../index.js")
 
-test("A multiplicação de 2 * 3 deve ser 6 ", () => {
+test("A multiplicação de 2 x 3 deve ser 6 ", () => {
     expect(multiplicar(2, 3)).toBe(6)
 })
