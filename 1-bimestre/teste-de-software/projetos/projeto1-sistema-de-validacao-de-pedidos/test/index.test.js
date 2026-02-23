465cd6cecb1c0d195f0025fe86c2d2a90390da02
Rosiele Sousa <rosielesousa49@gmail.com>
2026-02-23 17:46:03 +0000

    ajustes no projeto

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -4,7 +4,7 @@ test("pedido válido deve retornar true", () => {
     const pedidoValido = {
   cliente: "João",
   total: 100,
-  itens: ["produto 1"]
+  itens: ["produto 1", "produto 2"]
 }
     const pedido = validarPedido(pedidoValido)
     expect(pedido).toBe(true)
@@ -14,7 +14,7 @@ test("Pedido com o total de 0 deve retornar false", () => {
     const pedidoSemValor = {
   cliente: "João",
   total: 0,
-  itens: ["produto 1"]
+  itens: ["produto 1", "produto 2"]
 }
     const valor = validarPedido(pedidoSemValor)
     expect(valor).toBe(false)
@@ -23,7 +23,7 @@ test("Pedido com o total de 0 deve retornar false", () => {
 test("Pedido sem cliente deve retornar false", () => {
     const pedidoSemCliente = {
   total: 100,
-  itens: ["produto 1"]
+  itens: ["produto 1", "produto 2"]
 }
 
   const resultado = validarPedido(pedidoSemCliente)
